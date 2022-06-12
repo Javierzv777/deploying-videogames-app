ed6d52ac5ca9563688d80346a4144d8cee569d7d
Javier Zapata <javierzv777@gmail.com>
2022-06-12 09:53:42 -0500

    deploy with db try 8

--- src/db.js
+++ src/db.js
@@ -7,7 +7,9 @@ const {
 } = process.env;
 // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, ssl{
+  rejectUnauthorized:false
+}, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
